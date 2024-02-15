c3898ba5cd89abc92b1eab6bbeb9f4b9b6cc6045
Leonardo Godoy <leonardo.godoy@wildlifestudios.com>
2024-02-15 17:12:15 -0300

    chore: enabling pprof

--- main.go
+++ main.go
@@ -90,6 +90,7 @@ func main() {
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "a7b2d45c.cluster.x-k8s.io",
+		PprofBindAddress:       ":8081",
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
