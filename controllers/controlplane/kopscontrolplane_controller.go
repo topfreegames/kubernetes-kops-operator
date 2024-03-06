71fe4ad3682928b456feb8481ace2c27a2127b3a
rafatio <rafael.rafatio@gmail.com>
2024-03-06 16:25:51 -0300

    fix: remove unused parameters from function headers

--- controllers/controlplane/kopscontrolplane_controller.go
+++ controllers/controlplane/kopscontrolplane_controller.go
@@ -415,7 +415,7 @@ func (r *KopsControlPlaneReconciler) getClusterOwnerRef(ctx context.Context, obj
 	return nil, nil
 }
 
-func (r *KopsControlPlaneReconciler) reconcileKubeconfig(ctx context.Context, kubeConfig *rest.Config, kopsClientset simple.Clientset, kopsCluster *kopsapi.Cluster, cluster *unstructured.Unstructured) error {
+func (r *KopsControlPlaneReconciler) reconcileKubeconfig(ctx context.Context, kubeConfig *rest.Config, kopsCluster *kopsapi.Cluster, cluster *unstructured.Unstructured) error {
 
 	clusterName := cluster.GetName()
 
@@ -479,7 +479,7 @@ func (r *KopsControlPlaneReconciler) reconcileKubeconfig(ctx context.Context, ku
 	return nil
 }
 
-func (r *KopsControlPlaneReconciliation) reconcileClusterAddons(ctx context.Context, kopsClientset simple.Clientset, kopsCluster *kopsapi.Cluster) error {
+func (r *KopsControlPlaneReconciliation) reconcileClusterAddons(kopsClientset simple.Clientset, kopsCluster *kopsapi.Cluster) error {
 
 	if r.kcp.Spec.KopsClusterAddons == "" {
 		err := kopsClientset.AddonsFor(kopsCluster).Replace(nil)
@@ -739,7 +739,7 @@ func (r *KopsControlPlaneReconciler) Reconcile(ctx context.Context, req ctrl.Req
 		}
 	}
 
-	err = reconciler.reconcileClusterAddons(ctx, kopsClientset, kopsCluster)
+	err = reconciler.reconcileClusterAddons(kopsClientset, kopsCluster)
 	if err != nil {
 		return resultError, err
 	}
@@ -809,7 +809,7 @@ func (r *KopsControlPlaneReconciler) Reconcile(ctx context.Context, req ctrl.Req
 		return resultError, err
 	}
 
-	err = reconciler.reconcileKubeconfig(ctx, kubeConfig, kopsClientset, kopsCluster, owner)
+	err = reconciler.reconcileKubeconfig(ctx, kubeConfig, kopsCluster, owner)
 	if err != nil {
 		reconciler.Recorder.Eventf(kopsControlPlane, corev1.EventTypeWarning, "FailedToReconcileKubeconfig", "failed to reconcile kubeconfig: %s", err)
 		return resultError, err
@@ -826,7 +826,7 @@ func (r *KopsControlPlaneReconciler) Reconcile(ctx context.Context, req ctrl.Req
 	}
 	conditions.MarkTrue(kopsControlPlane, controlplanev1alpha1.TerraformApplyReadyCondition)
 
-	err = reconciler.updateKopsMachinePoolWithProviderIDList(ctx, kopsControlPlane, kmps, &reconciler.awsCredentials)
+	err = reconciler.updateKopsMachinePoolWithProviderIDList(kopsControlPlane, kmps, &reconciler.awsCredentials)
 	if err != nil {
 		if apierrors.IsNotFound(err) {
 			return requeue1min, nil
@@ -857,7 +857,7 @@ func (r *KopsControlPlaneReconciler) Reconcile(ctx context.Context, req ctrl.Req
 	return resultDefault, nil
 }
 
-func (r *KopsControlPlaneReconciliation) updateKopsMachinePoolWithProviderIDList(ctx context.Context, kopsControlPlane *controlplanev1alpha1.KopsControlPlane, kmps []infrastructurev1alpha1.KopsMachinePool, credentials *aws.Credentials) error {
+func (r *KopsControlPlaneReconciliation) updateKopsMachinePoolWithProviderIDList(kopsControlPlane *controlplanev1alpha1.KopsControlPlane, kmps []infrastructurev1alpha1.KopsMachinePool, credentials *aws.Credentials) error {
 	for i, kopsMachinePool := range kmps {
 		// TODO: retrieve karpenter providerIDList
 		if len(kopsMachinePool.Spec.SpotInstOptions) == 0 && kopsMachinePool.Spec.KopsInstanceGroupSpec.Manager != "Karpenter" {
